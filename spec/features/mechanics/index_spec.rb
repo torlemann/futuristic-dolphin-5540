f3e556920be86ade1a6a7047925fa987583d256b
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-09-12 09:45:14 -0700

    Add avg yrs xp test to model and feat: add mechanics index page

--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'Mechanics Index' do
     before :each do
-        @park = AmusementPark.create! ( name: "Politically Incorrect Park", admission_cost: 75 )
+        @park = AmusementPark.create!( name: "Politically Incorrect Park", admission_cost: 75 )
 
-        @mechanic_1 = Mechanic.create!( name: "Al", years_of_experience: 40)
-        @mechanic_2 = Mechanic.create!( name: "Dirty Burt", years_of_experience: 25)
-        @mechanic_3 = Mechanic.create!( name: "Spenser", years_of_experience: 1.5)
+        @mechanic_1 = Mechanic.create!( name: "Al", years_experience: 40)
+        @mechanic_2 = Mechanic.create!( name: "Dirty Burt", years_experience: 25)
+        @mechanic_3 = Mechanic.create!( name: "Spenser", years_experience: 1)
 
         # @ride_1 = Ride.create!( name: "The Death Wish", thrill_rating: 11, open: false, amusement_park_id: @park.id )
         # @ride_2 = Ride.create!( name: "Whirling Dervish", thrill_rating: 5, open: false, amusement_park_id: @park.id )
@@ -19,19 +19,20 @@ RSpec.describe 'Mechanics Index' do
         visit '/mechanics'
     end
 
-    it 'lists names and xp of all mechanics' do
+    it 'has a header and lists names and xp of all mechanics' do
+        expect(page).to have_content("All Mechanics")
         within "#mechanic-#{@mechanic_1.id}" do
             expect(page).to have_content(@mechanic_1.name)
-            expect(page).to have_content(@mechanic_1.years_of_experience)
+            expect(page).to have_content(@mechanic_1.years_experience)
         end
         within "#mechanic-#{@mechanic_2.id}" do
             expect(page).to have_content(@mechanic_2.name)
-            expect(page).to have_content(@mechanic_2.years_of_experience)          
+            expect(page).to have_content(@mechanic_2.years_experience)          
         end
     end
 
     it 'shows the avg years xp of all mechanics' do
-        expect(page).to have_content("Average Experience: 22.16 years")          
+        expect(page).to have_content("Average Experience: 22.0 years")        
     end
 
 end
--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe Mechanic, type: :model do
     it { should have_many :mechanic_rides }
     it { should have_many(:rides).through(:mechanic_rides) }
   end
+
+  it 'can calculate the average xp of mechanics' do
+    @mechanic_1 = Mechanic.create!( name: "Al", years_experience: 40)
+    @mechanic_2 = Mechanic.create!( name: "Dirty Burt", years_experience: 25)
+    
+    expect(Mechanic.average_xp).to eq(32.5)
+  end
 end
